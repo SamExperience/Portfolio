3031b052d7893580a9a172cb01ae1ffa49091ded
Samuele Deriu <deriusamuele@gmail.com>
2025-10-03 11:20:45 +0200

    feat: add projects section with 3 sample cards

--- src/app/components/projects-list/projects-list.ts
+++ src/app/components/projects-list/projects-list.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { ProjectCard } from '../project-card/project-card';
 @Component({
   selector: 'app-projects-list',
   standalone: true,
-  imports: [],
+  imports: [ProjectCard],
   templateUrl: './projects-list.html',
   styleUrl: './projects-list.scss',
 })
