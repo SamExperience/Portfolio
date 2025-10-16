e82dc85db4ddaf1a8bf1b14ca8dd471f6905904e
Samuele Deriu <deriusamuele@gmail.com>
2025-10-16 11:41:19 +0200

    chore(scss styles): chore: separate styles into dedicated SCSS files for each component

--- src/app/components/content/content.ts
+++ src/app/components/content/content.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero/hero';
 import { ProjectsList } from '../projects-list/projects-list';
-import { ProjectCard } from '../project-card/project-card';
 import { About } from '../about/about';
 /* COMPONENTS */
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [Hero, ProjectsList, ProjectCard, About],
+  imports: [Hero, ProjectsList, About],
   templateUrl: './content.html',
   styleUrl: './content.scss',
 })
