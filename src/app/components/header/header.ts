fb6599aa43133dee2e6647e4bf8a4f8f38c7d32d
Samuele Deriu <deriusamuele@gmail.com>
2025-10-17 18:23:04 +0200

    chore:Add new mobile menu component with initial structure and SCSS, update header accordingly

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuIconComponent } from '../menu-icon/menu-icon';
 import { CommonModule } from '@angular/common';
-
+import { MenuMobile } from '../menu-mobile/menu-mobile';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MenuIconComponent],
+  imports: [CommonModule, MenuIconComponent, MenuMobile],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
---
+++ src/app/components/menu-mobile/menu-mobile.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-menu-mobile',
+  imports: [],
+  templateUrl: './menu-mobile.html',
+  styleUrl: './menu-mobile.scss',
+})
+export class MenuMobile {}
